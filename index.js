d699e12590dde08bdb039f7ae6a50248415927b3
Stefan Reeves <stefanreeves24@gmail.com>
2024-06-03 12:03:28 -0700

    add console.log process.env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5050;
+console.log("process.eng", process.env);
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my first Heroku App</h1>`);
